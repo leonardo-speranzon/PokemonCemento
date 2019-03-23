bb6f5d506a176ce6a9caf39e56160a50514e7c0e
Leonardo Speranzon <leonardo.speranzon@gmail.com>
2019-03-23 17:54:39 +0100

    Roba minore -javadoc

--- app/src/main/java/it/speranzon_galligioni/pokemoncemento/gameObject/Player.java
+++ app/src/main/java/it/speranzon_galligioni/pokemoncemento/gameObject/Player.java
@@ -7,6 +7,12 @@ import it.speranzon_galligioni.pokemoncemento.R;
 public class Player extends GameElement {
 	private boolean blocked;
 
+	/**
+	 * Costruttore di Player
+	 * @param context Context
+	 * @param x coordinata X
+	 * @param y coordinata Y
+	 */
 	public Player(Context context, int x, int y) {
 		super(context, x, y, 1, 1, R.drawable.player_0);
 	}
